1d00b1bcd10656e716edbddf44c8d34fc7719cd9
Enoch2k2 <enoch2k2@hotmail.com>
2023-05-03 14:21:56 -0500

    finished

--- server/app.py
+++ server/app.py
@@ -19,15 +19,46 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    response_body = f"""
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>
+    """
+
+    return make_response(response_body, 200)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    animals = "".join([f'<ul>Animal: {animal.name}</ul>' for animal in zookeeper.animals])
+
+    response_body = f"""
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+        { animals }
+    """
+    return make_response(response_body, 200)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    animals = "".join([f'<ul>Animal: {animal.name}</ul>' for animal in enclosure.animals])
+
+    response_body = f"""
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open To Visitors: {enclosure.open_to_visitors}</ul>
+        { animals }
+    """
+
+    return make_response(response_body, 200)    
 
 
 if __name__ == '__main__':
